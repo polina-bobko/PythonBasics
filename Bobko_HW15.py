775a9c375aca0ca4d340c031b5c66189b574fda3
Polina Bobko <50700267+polina-bobko@users.noreply.github.com>
2026-03-01 20:43:40 +0100

    Enhance product pricing and output formatting
    Added print statement for processed one-word list and added calculated new price for each product after applying a discount.

--- Bobko_HW15.py
+++ Bobko_HW15.py
@@ -4,7 +4,7 @@ one_words_list = []
 for st in text_list:
     if " " not in st:
         one_words_list.append(st.lower())
-print(one_words_list)
+print("Обработанный список:", one_words_list)
 
 # Обновление цен товаров
 products = [["Laptop", 1200], ["Mouse", 25], ["Keyboard", 75], ["Monitor", 200]]
@@ -13,4 +13,5 @@ print(f"{'Товар':<15}{'Старая цена':^15}{'Новая цена':<1
 for item in products:
     product, price = item
     new_price = price-discount*price/100
+    item.append(new_price)
     print(f"{product:<15}{(f'{price:.2f}$'):^15}{(f'{new_price:.2f}$'):<15}")
